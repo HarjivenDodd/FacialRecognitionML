c96793bd0afad8e20dbe558b4ba221f0398fe160
HarjivenDodd <129721024+HarjivenDodd@users.noreply.github.com>
2024-06-18 15:17:53 -0700

    Added csv recording to testMassIdentification
    added additional images and csv recording functionality

--- testMassIdentification.py
+++ testMassIdentification.py
@@ -1,5 +1,6 @@
 import face_recognition
 import numpy as np
+from datetime import datetime
 import cv2
 import os
 
@@ -25,33 +26,49 @@ def findEncodings(images):
 encodeListKnown = findEncodings(images)
 print('Encodings Complete')
 
-cap = cv2.VideoCapture(0)
+# records the name of the person in the image and the time the person was identified
+def recordName(name):
+    with open('names.csv','r+') as f:
+        dataList = f.readlines()
+        names =[]
+        for line in dataList:
+            entry = line.split(',')
+            names.append(entry[0])
+            if name not in names:
+                now = datetime.now()
+                dt_string = now.strftime("%H:%M:%S")
+                f.writelines(f'\n{name},{dt_string}')
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
-     success, img = cap.read()
-     imgs = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) # reduces the size of images from the webcam to improve processing speed
-     imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB) # converts to RGB
+    success, img = cap.read()
+    imgt = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) # reduces the size of images from the webcam to improve processing speed
+    imgt = cv2.cvtColor(imgt, cv2.COLOR_BGR2RGB) # converts to RGB
 
-     facesCurFrame = face_recognition.face_locations(imgs)
-     encodesCurFrame = face_recognition.face_encodings(imgs, facesCurFrame)
-     # locates face in images and uses this location to encode the face in the image
+    facesCurFrame = face_recognition.face_locations(imgt)
+    encodesCurFrame = face_recognition.face_encodings(imgt, facesCurFrame)
+    # locates face in images and uses this location to encode the face in the image
 
-     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-          matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-          faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-     # compares the face in the image to the faces in the images list for current frame
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+    # compares the face in the image to the faces in the images list for current frame
+        matchIndex = np.argmin(faceDis)
 
-     matchIndex = np.argmin(faceDis)
+        if faceDis[matchIndex]< 0.50:
+            name = className[matchIndex].upper()
+            recordName(name)
+        else: 
+            name = 'Unidentified'
+        y1,x2,y2,x1=faceLoc
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # multiplications are used to adjust the rectangles to the appropriate size of original image
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+    # determines the closest match and displays the name of the person on the image, as well as rectangles around the face and the name
+    # also rejects the face if the confidence level is less than 0.5 and displays 'Unidentified'
 
-     if matches[matchIndex]:
-         name = className[matchIndex].upper()
-         y1,x2,y2,x1=faceLoc
-         y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # multiplications are used to adjust the rectangles to the appropriate size of original image
-         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-         cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-         cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
-     # determines the closest match and displays the name of the person on the image, as well as rectangles around the face and the name
-
-cv2.imshow('Webcam', img)
-cv2.waitKey(1)
-
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+    # shows the webcam feed and the name of the person identified, or 'Unidentified' if the confidence level is less than 0.5
